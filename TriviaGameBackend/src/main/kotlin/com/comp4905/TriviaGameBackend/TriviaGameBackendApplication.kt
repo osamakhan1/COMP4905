95c814bd95373f2829cee33c6c9fb7e1f208bc9e
osamakhan <osama.a.khan97@gmail.com>
2021-01-31 14:21:06 -0500

    [Setup]- Base app and server code

---
+++ TriviaGameBackend/src/main/kotlin/com/comp4905/TriviaGameBackend/TriviaGameBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.comp4905.TriviaGameBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TriviaGameBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<TriviaGameBackendApplication>(*args)
+}
---
+++ TriviaGameBackend/src/test/kotlin/com/comp4905/TriviaGameBackend/TriviaGameBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.comp4905.TriviaGameBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TriviaGameBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
